152b28065e37f3af3bd5976867e0b8d54dd64cf9
Suresh Mudalla <sureshmudalla1144@gmail.com>
2025-07-01 10:17:04 +0530

    Time: 1 ms (99.9%), Space: 44.2 MB (5.48%) - LeetHub

--- 0090-subsets-ii/0090-subsets-ii.java
+++ 0090-subsets-ii/0090-subsets-ii.java
@@ -1,27 +1,23 @@
+import java.util.*;
+
 class Solution {
-    void powerset(int index,int[] nums, List<Integer> current, List<List<Integer>> ans)
-    {
-         ans.add(new ArrayList<>(current));
-        for(int i=index;i<nums.length;i++)
-        {
-        if(i>index && nums[i]==nums[i-1])
-         {
-            continue;
+    public void Uniqueset(int start, int[] nums, List<Integer> current, List<List<Integer>> ans) {
+        ans.add(new ArrayList<>(current));
+
+        for (int i = start; i < nums.length; i++) {
+           
+            if (i > start && nums[i] == nums[i - 1]) continue;
+
+            current.add(nums[i]);
+            Uniqueset(i + 1, nums, current, ans);
+            current.remove(current.size() - 1);
         }
-        current.add(nums[i]);
-
-        powerset(i+1, nums, current, ans);
-
-        current.remove(current.size()-1);
-       
     }
 
-    }
-     public List<List<Integer>> subsetsWithDup(int[] nums) {
-        List<List<Integer>> result = new ArrayList<>();
-        List<Integer> current = new ArrayList<>();
-            Arrays.sort(nums);
-        powerset(0, nums, current, result);
-        return result;
+    public List<List<Integer>> subsetsWithDup(int[] nums) {
+        List<List<Integer>> ans = new ArrayList<>();
+        Arrays.sort(nums);  
+        Uniqueset(0, nums, new ArrayList<>(), ans);
+        return ans;
     }
 }
